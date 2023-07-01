3e44657286242f37fcf9f114e835fc30d0bd68dc
James Dorevski <github@jamesdorevski.com>
2023-07-02 07:32:27 +1000

    some

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,18 @@ console.log('Hello World, from TypeScript!');
 import 'graphql-import-node';
 import { execute, parse } from 'graphql';
 import { schema } from './schema';
+import fastify from 'fastify';
 
 async function main() {
-  const query = parse('query { info }');
+  const server = fastify();
 
-  const result = await execute({
-    schema,
-    document: query
+  server.get('/', (req, resp) => {
+    resp.send({ test: true});
   });
 
-  console.log(result);
+  server.listen(8080, "0.0.0.0", () => {
+    console.log('Server is running on http://localhost:8080/');
+  });
 }
 
 main();
