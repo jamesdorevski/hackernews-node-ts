ae2845d97e7c75ffb257eacbb4e715d2869b7a41
James Dorevski <github@jamesdorevski.com>
2023-07-02 12:29:23 +1000

    added feed query

--- src/schema.ts
+++ src/schema.ts
@@ -1,13 +1,32 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import typeDefs from './schema.graphql';
 
+type Link = {
+    id: string;
+    url: string;
+    description: string;
+}
+
+// DEBUG: dummy data
+const links: Link[] = [{
+    id: "link-0",
+    url: "www.example.com",
+    description: "Create commons webpage",
+}]
+
 const resolvers = {
-  Query: {
-    info: () => 'Test',
-  }
+    Query: {
+        info: () => 'Test',
+        feed: () => links,
+    },
+    Link: {
+        id: (parent: Link) => parent.id,
+        description: (parent: Link) => parent.description,
+        url: (parent: Link) => parent.url,
+    }
 }
 
 export const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
 });
