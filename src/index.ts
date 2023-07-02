45a1824e1de073b8aa20b8f2e3d9fd55a560931b
James Dorevski <github@jamesdorevski.com>
2023-07-02 12:06:36 +1000

    added GraphiQL

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,57 @@
 console.log('Hello World, from TypeScript!');
 
 import 'graphql-import-node';
-import { execute, parse } from 'graphql';
+import { getGraphQLParameters, processRequest, Request, sendResult, renderGraphiQL, shouldRenderGraphiQL } from 'graphql-helix';
 import { schema } from './schema';
 import fastify from 'fastify';
+import { request } from 'http';
 
 async function main() {
-  const server = fastify();
+    const server = fastify();
 
-  server.get('/', (req, resp) => {
-    resp.send({ test: true});
-  });
+    server.get('/', (req, resp) => {
+        resp.send({ test: true});
+    });
 
-  server.listen(8080, "0.0.0.0", () => {
-    console.log('Server is running on http://localhost:8080/');
-  });
+    server.route({
+        method: ["POST", "GET"],
+        url: "/graphql",
+        handler: async (req, resp) => {
+            const request: Request = {
+                headers: req.headers,
+                method: req.method,
+                query: req.query,
+                body: req.body
+            };
+
+            if (shouldRenderGraphiQL(request)) {
+                resp.header("Content-Type", "text/html");
+                resp.send(
+                    renderGraphiQL({
+                        endpoint: "/graphql",
+                    })
+                );
+
+                return;
+            }
+
+            const { operationName, query, variables } = getGraphQLParameters(request);
+
+            const result = await processRequest({
+                request,
+                schema,
+                operationName,
+                query,
+                variables,
+            });
+
+            sendResult(result, resp.raw);
+        }
+    });
+
+    server.listen(8080, "0.0.0.0", () => {
+        console.log('Server is running on http://localhost:8080/');
+    });
 }
 
 main();
